87ed3718c657af5b8714bbe1deff4bb5de48f174
Priya <sharma27598@gmail.com>
2023-01-18 13:39:40 +0530

    Initial commit atm1

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int atmnumber = 12345;
+        int atmpin = 123;
+        Scanner scan = new Scanner(System.in);
+        System.out.println(" *************** WELCOME TO ATM MACHINE *************** ");
+        System.out.println("Enter the ATM Number : ");
+        int atmNumber = scan.nextInt();
+        System.out.println("Enter the ATM Pin : ");
+        int pin = scan.nextInt();
+        if((atmnumber==atmNumber)&&(atmpin==pin))
+        {
+            while(true){
+                System.out.println("1.View Available Balance\n2.Withdraw Amount\n3.Deposit Amount\n4.View Mini Statement\n5.Exit");
+                System.out.println("Enter Choice : ");
+                int ch = scan.nextInt();
+                if(ch==1){
+
+                }
+                else if(ch==2)
+                {
+
+                }
+                else if(ch==3)
+                {
+
+                }
+                else if(ch==4)
+                {
+
+                }
+                else if(ch==5)
+                {
+                    System.out.println("Collect your ATM Card\n Thank you for using ATM Machine");
+                    System.exit(0);
+                }
+                else{
+                    System.out.println("Please enter correct choice");
+                }
+            }
+        }
+
+        {
+            System.out.println("Incorrect ATM Number or Pin..!!");
+            System.exit(0);
+        }
+    }
+}
